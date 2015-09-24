6ef9220716c6b47c3b780bb9c2ddccb3e5bb9bcb
inet2005 <38High36>
2015-09-24 00:26:47 -0300

    Lab 2 - Steps 1,2,3 all done. Part 4 and Section B still needed

--- Lab2/Lab2.php
+++ Lab2/Lab2.php
@@ -4,12 +4,49 @@
  * User: inet2005
  * Date: 9/23/15
  * Time: 2:54 PM
+ * @param $H
+ * @param $message
  */
 
 function createHeading($H, $message) {
     if($H >= 1 && $H <= 6) {
         echo "<h$H>$message</h$H>";
+    } else {
+        echo "error this h tag is outside the bounds of 1-6";
     }
-}
+}//end createHeading
+?>
+
+<h3>Step #1</h3>
+
+<?php
+for($i=1; $i<8; $i++) {
+    createHeading($i, "This is a h" . $i . " heading");
+}// end for loop
+?>
+
+<h3>Step #2</h3>
+
+<?php
+$myString = "Hello World";
+function appendByVal($s) {
+    $s .= "...blah";
+}// end appendByVal
+
+function appendByRef(&$s) {
+    $s .= "...blah";
+}// end appendByRef
+
+echo $myString . "<br />";
+appendByVal($myString);
+echo $myString . "<br />";
+appendByRef($myString);
+echo $myString . "<br />";
+?>
+
+<h3>Step #3</h3>
+
+<?php
+
 
 ?>
