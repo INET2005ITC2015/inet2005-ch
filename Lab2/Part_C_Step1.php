74bdecfaa6e4e0ca6f3ff8fb5f78b4672359e5fc
inet2005 <38High36>
2015-09-25 00:56:39 -0300

    Lab 2 - Final, unsure if file uploader works because the cancerous piece of garbage that is PhpStorm will not run its' debugger.

--- Lab2/Part_C_Step1.php
+++ Lab2/Part_C_Step1.php
@@ -12,8 +12,15 @@ $feet = $_POST['hFeet'];
 $inches = $_POST['hInches'];
 $meters = 0.0254 * ((12 * $feet) + $inches);
 
+$fileTmpName = $_FILES['userFile']['tmp_name'];
+$fileOrigName = $_FILES['userFile']['orig_name'];
+$fileSize = $_FILES['userFile']['size'];
+$fileErrorUpload = $_FILES['userFile']['error'];
+$result = move_uploaded_file($fileTmpName, "uploads/" . $fileOrigName);
+
 echo "<h3>Your first name is $firstName</h3>";
 echo "<h3>Your last name is $lastName</h3>";
-echo "<h3>Your height in meters is $meters</h3>";
+echo "<h3>Your height in meters is $meters m</h3>";
+echo "<h3>TMP: $fileTmpName <br /> Orig: $fileOrigName <br /> Size: $fileSize <br /> Error: $fileErrorUpload</h3>";
 
 ?>
