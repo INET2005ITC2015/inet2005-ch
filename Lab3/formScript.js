b5545dc1422e98f406669a3eb46c963ab0a4cf29
inet2005 <38High36>
2015-10-05 20:49:58 -0300

    PHP Lab #3 - Form JS completed

--- Lab3/formScript.js
+++ Lab3/formScript.js
@@ -1,25 +1,54 @@
 "use strict";
 
-var textBoxes = document.getElementsByClassName("textBox");
-
-
 function focusColor(myElement) {
+    myElement.parentNode.style.textDecoration = "underline";
     myElement.style.backgroundColor = "#FFFF99";
+    myElement.style.fontStyle = "italic";
+
 }
 
 function blurColor(myElement) {
+    myElement.parentNode.style.textDecoration = "none";
     myElement.style.backgroundColor = "#FFFFFF";
+    myElement.style.fontStyle = "normal";
 }
 
+function validForm() {
+    var formIsValid = true;
+    for (var i = 0; i < textBoxes.length; i++) {
+        if (textBoxes[i].value === "") {
+            formIsValid = false;
+            textBoxes[i].style.borderColor = "red";
+        }
+    }
+    if (!termsAgreement.checked) {
+        formIsValid = false;
+        document.getElementById("termsError").style.visibility = "visible";
+    }
+    return formIsValid;
+}
+
+var textBoxes = document.getElementsByClassName("textBox");
+var termsAgreement = document.getElementById("termsAccept");
+
 for (var i = 0; i < textBoxes.length; i++) {
-    textBoxes[i].addEventListener("focus", function() {
+    console.log(i);
+    textBoxes[i].addEventListener("click", function () {
         focusColor(this);
-        console.log("listener added");
-    })
+    });
 }
 
 for (var i = 0; i < textBoxes.length; i++) {
-    textBoxes[i].addEventListener("blur", function() {
+    textBoxes[i].addEventListener("blur", function () {
         blurColor(this);
-    })
+    });
 }
+
+// WILL NOT WORK!!!!!
+//document.getElementById("testForm").addEventListener("submit", function () {
+//    if(validForm()) {
+//        return true;
+//    } else {
+//        return false;
+//    }
+//});
