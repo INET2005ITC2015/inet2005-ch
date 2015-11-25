a6d5610fd917af3c70ca3f0f6a703e63486337f0
inet2005 <38High36>
2015-11-25 06:01:26 -0400

    Lab8 - Step 5 Complete

---
+++ Lab8/resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+
+@section('content')
+    <h1>About Me: {{ $first }} {{ $last }}</h1>
+    <p>Alsjflkjljsadlfjlkj ldjldskjlj flflakjsljwhweujf l lfj ifj iaj lsj lakjf lkajljglkgj lk j</p>
+@stop
